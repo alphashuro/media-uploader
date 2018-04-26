7c8f8f44d0672d5079b001616e5708b2cd5e069e
Alpha Shuro <alphashuro@gmail.com>
2018-04-26 18:03:48 +0200

    use config and args

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,89 @@
+#[macro_use]
+extern crate clap;
 extern crate reqwest;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate toml;
 
+use std::io::{Read, Write};
 use std::env;
 use std::fs::{self, File, OpenOptions};
 use std::path::PathBuf;
 use std::thread;
 
 fn main() {
-    let (media_url, folder) = get_args();
-    let results_file: File = open_results_file();
+    let matches = clap_app!(Uploader =>
+        (version: "1.0")
+        (author: "Alpha Shuro")
+        (about: "Uploads media to the media service a la bluerobot")
+        (@arg folder: -f --folder +takes_value "Folder to upload") 
+        (@subcommand set =>
+            (about: "Sets config options")
+            (version: "1.0")
+            (@arg url: -u --url +required +takes_value "Media Service URL")
+        )
+    ).get_matches();
 
-    let files: Vec<PathBuf> = read_dir(folder);
-    let results: Vec<Media> = upload_files(media_url, files);
+    if let Some(folder) = matches.value_of("folder") {
+        let config = read_config_file();
+        let media_url = config.media_url;
 
-    write(results_file, results)
+        let folder = matches.value_of("folder").unwrap();
+        let results_file: File = open_results_file();
+
+        let files: Vec<PathBuf> = read_dir(folder.to_owned());
+        let results: Vec<Media> = upload_files(media_url, files);
+
+        write(results_file, results);
+    }
+
+    if let Some(matches) = matches.subcommand_matches("set") {
+        if matches.is_present("url") {
+            let url = matches.value_of("url").unwrap();
+            let config = Config {
+                media_url: url.to_owned(),
+            };
+            set_config(config);
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    media_url: String,
+}
+
+fn get_config_path() -> PathBuf {
+    let (_, home_folder) = env::vars()
+        .find(|&(ref key, _)| key == "HOME")
+        .expect("HOME not defined!!");
+
+    let mut path = PathBuf::from(home_folder);
+    path.push(".uploader");
+
+    path
+}
+
+fn read_config_file() -> Config {
+    let path = get_config_path();
+    let mut file = File::open(path).expect("Could not open config file");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Could not read config file");
+
+    toml::from_str(&contents).expect("Failed to parse config as toml")
+}
+
+fn set_config(config: Config) {
+    let path = get_config_path();
+    let toml = toml::to_string(&config).unwrap();
+
+    let mut f = File::create(path).expect("Could not create config file");
+    f.write_all(toml.as_bytes())
+        .expect("Could not write config file");
 }
 
 fn write(file: File, data: Vec<Media>) {
@@ -33,12 +100,11 @@ fn read_dir(folder: String) -> Vec<PathBuf> {
         .collect()
 }
 
-fn get_args() -> (String, String) {
+fn get_args() -> String {
     let mut args = env::args().skip(1);
-    let media_url = args.next().expect("Usage: ./upload [media_url] [folder]");
-    let folder = args.next().expect("Usage: ./upload [media_url] [folder]");
+    let folder = args.next().expect("Usage: ./upload [folder]");
 
-    (media_url, folder)
+    folder
 }
 
 #[derive(Debug, Serialize, Deserialize)]
