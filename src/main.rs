ddd201f2eef86c54752dc0e6652f3b2618eb147f
Alpha Shuro <alphashuro@gmail.com>
2018-04-26 15:53:22 +0200

    fix form file name

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,42 @@ extern crate serde_json;
 
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::thread;
-use std::io::Write;
+
+fn main() {
+    let mut args = env::args();
+    let _name = args.next();
+    let media_url = args.next().expect("Usage: ./upload [media_url] [folder]");
+    let folder = args.next().expect("Usage: ./upload [media_url] [folder]");
+    let folder = Path::new(&folder);
+
+    let results_file = fs::OpenOptions::new()
+        .write(true)
+        .append(false)
+        .create(true)
+        .open("results.json")
+        .expect("Could not create/update results.json file");
+
+    let files: Vec<PathBuf> = fs::read_dir(folder)
+        .expect("Failed to read folder")
+        .map(|file| file.unwrap().path())
+        .collect();
+
+    let results: Vec<Vec<Media>> = files
+        .chunks(10)
+        .map(|paths| {
+            let url = media_url.clone();
+            let aths = paths.to_owned();
+
+            thread::spawn(move || upload(url, paths))
+        })
+        .map(|handle| handle.join().unwrap())
+        .collect();
+
+    serde_json::to_writer_pretty(results_file, &results)
+        .expect("Failed to convert upload results to JSON and write to file");
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Media {
@@ -18,54 +51,14 @@ struct Media {
     size: i64,
 }
 
-fn main() {
-    let mut args = env::args();
-    let _name = args.next();
-    let media_url = args.next().expect("Usage: ./upload [media_url] [folder]");
-    let folder = args.next().expect("Usage: ./upload [media_url] [folder]");
-    let folder = Path::new(&folder);
-
-    let mut file = fs::OpenOptions::new()
-        .write(true)
-        .append(false)
-        .create(true)
-        .open("results.json")
-        .expect("Could not create/update results.json file");
-
-    let files = fs::read_dir(folder).expect("Failed to read folder");
-
-    let paths: Vec<std::path::PathBuf> = files
-        .map(|file| {
-            let file = file.unwrap();
-            file.path()
-        })
-        .collect();
-
-    let results: Vec<Vec<Media>> = paths
-        .chunks(2)
-        .map(|paths| {
-            let url = media_url.clone();
-            let paths = paths.to_owned();
-
-            thread::spawn(move || upload(url, paths))
-        })
-        .map(|handle| handle.join().unwrap())
-        .collect();
-
-    let json = serde_json::to_string(&results).expect("Failed to convert upload results to JSON");
-
-    match file.write_all(json.as_bytes()) {
-        Ok(_) => println!("results file written"),
-        Err(e) => println!("{}", e),
-    }
-}
-
-fn upload(url: String, paths: Vec<std::path::PathBuf>) -> Vec<Media> {
+fn upload(url: String, paths: Vec<PathBuf>) -> Vec<Media> {
     let client = reqwest::Client::new();
     let form = reqwest::multipart::Form::new();
 
     let params = paths.iter().fold(form, |form, ref path| {
         let name = path.file_name().unwrap().to_str().unwrap().to_owned();
+
+        println!("{}", name);
 
         form.file(name, path).unwrap()
     });
